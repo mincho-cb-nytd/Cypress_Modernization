53fb1169cc97eb634346696262122d88b20e55b6
mincho-cb-nytd <150717200+mincho-cb-nytd@users.noreply.github.com>
2024-07-01 12:24:59 -0400

    fedderalUserTabValidation

--- cypress/e2e/HomePage/HomePage.js
+++ cypress/e2e/HomePage/HomePage.js
@@ -6,18 +6,26 @@ describe("Login Page Validations", function () {
  
   it("Verify user is able to select state user tab", function () {
     cy.visit("");
-    homePage.clickOnStateButton();
+    homePage.clickOnStateUserTab();
     homePage.elements.loginInstructionsMsg().contains('Please enter your Username and Password and select Login to begin using the NYTD portal')
     homePage.elements.usernameText().contains('Username')
     homePage.elements.forgotUsernameText().contains('Forgot Username')
     homePage.elements.passwordText().contains('Password')
     homePage.elements.forgotPasswordText().contains('Forgot Password')
-    homePage.elements.LoginBtn().contains('Login')
+    homePage.elements.StateUserLoginBtn().should('have.text','Login')
+  });
+  
+  it("Verify user is able to select federal user tab", function () {
+    cy.visit("");
+    homePage.clickOnFederalUserTab();
+    homePage.elements.federalUserLogInInstructionText().should('have.text','Insert your HSPD-12 access card into the smart card reader before you select login.');
+    homePage.elements.pavCardImage().should('be.visible').and('have.prop', 'naturalWidth').then(cy.log).should('be.greaterThan',0);
+    homePage.elements.FederalUserLoginBtn().should('have.text','Login');
   });
 
   it("Verify user is able to enter text for username and password", function () {
     cy.visit("");
-    homePage.clickOnStateButton();
+    homePage.clickOnStateUserTab();
     homePage.verifyUsernameAndPasswordInputContainsSpecificValue();
   });
 
@@ -36,8 +44,8 @@ describe("Login Page Validations", function () {
   it("Verify Signing in with incorrect username will display error validation message Incorrect username or password. Please try again", function () {
     cy.visit("");
     homePage.enterIncorrectUsernameAndPassword();
-    homePage.clickOnLoginBtn();
-    homePage.elements.incorrectUsernameAndPasswordErrorMsg().contains('Incorrect username or password. Please try again.')
+    homePage.clickOnStateUserLoginBtn();
+    homePage.elements.incorrectUsernameAndPasswordErrorMsg().should('have.text','Incorrect username or password. Please try again.')
 
   });
 });
--- cypress/e2e/pages/HomePageObjects.js
+++ cypress/e2e/pages/HomePageObjects.js
@@ -3,7 +3,8 @@ class HomePageObjects {
         nytdIcon: () => cy.get('[data-testid="NYTD_logo_left"]'), // NYTD Icon 
         phoneNumberLink: () => cy.get('[data-testid="phone_number_link"]'), // Phone number link to deal NYTD Help desk.
         emailLink: () => cy.get('[data-testid="email_link"]'), // Email link to sent email to Help desk .
-        stateUserBtn: () => cy.get('[data-testid="state_user_button"]'), // State user tab for log in with username and password.
+        stateUserTab: () => cy.get('[data-testid="state_user_button"]'), // State user tab for log in with username and password.
+        federalUserTab: () => cy.get('[class="usa-button styles_inactiveButton__sAFAc"]'), // Federal user tab for log in with PAV Card
         loginInstructionsMsg: () => cy.get('[data-testid="state_instructions"]'), // Login instructions message text "Please enter your Username and Password and select Login to begin using the NYTD portal".
         usernameText: () => cy.get('.styles_formGroup1___KxLX > [data-testid="label"]'), // Username* text above the username input.
         usernameInput: () => cy.get('[data-testid="user_id_input"]'), // Username input, where to enter username.
@@ -11,10 +12,13 @@ class HomePageObjects {
         passwordText: () => cy.get('.styles_formGroup2___aa7w > [data-testid="label"]'), // Password* text above the password input.
         passwordInput: () => cy.get('[data-testid="password_input"]'), // Password input where to enter password. 
         forgotPasswordText: () => cy.get('[data-testid="forgot_password_link"]'), // Forgot password link under the password input witch is navigating to forgot password page.
-        LoginBtn: () => cy.get('[data-testid="state_user_login_button"]'), // Login button 
+        StateUserLoginBtn: () => cy.get('[data-testid="state_user_login_button"]'), // Login button for State User
+        FederalUserLoginBtn: () => cy.get('[data-testid="federal_submit"]'), // Login button for FederalUser User
         incorrectUsernameAndPasswordErrorMsg: () => cy.get('#validation_message'), // Error msg "Incorrect username or password. Please try again." when user enters incorrect username or password.
-
+        federalUserLogInInstructionText: () => cy.get('[data-testid="federal_instructions"]'), // Federal user instruction text witch display after clicking on federal user tab.
+        pavCardImage: () => cy.get('[data-testid="federal_card_image"]'), // Card image witch display after clicking on federal user tab
     }
+  
 
     clickOnNYTDIcon() {
         this.elements.nytdIcon().invoke('removeAttr', 'target', '_blank').click();
@@ -28,8 +32,11 @@ class HomePageObjects {
         this.elements.email().click();
     }
 
-    clickOnStateButton() {
-        this.elements.stateUserBtn().click();
+    clickOnStateUserTab() {
+        this.elements.stateUserTab().click();
+    }
+    clickOnFederalUserTab() {
+        this.elements.federalUserTab().click();
     }
 
     clickOnForgotUsername() {
@@ -47,8 +54,8 @@ class HomePageObjects {
         this.elements.usernameInput().type('abcdef').should('have.value', 'abcdef').clear();
         this.elements.passwordInput().type(12345).should('have.value', '12345').clear();
     }
-    clickOnLoginBtn() {
-        this.elements.LoginBtn().click();
+    clickOnStateUserLoginBtn() {
+        this.elements.StateUserLoginBtn().click();
     }
     enterUsernameAndPassword(username, password) {
         this.elements.usernameInput().type(username, { log: false });
